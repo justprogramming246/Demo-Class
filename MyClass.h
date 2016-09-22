804747c7cb43b4c0105e786e92d7d78e41564394
justprogramming246 <justprogramming246@users.noreply.github.com>
2016-09-22 13:48:45 -0400

    Filled in missing parts of header

--- MyClass.h
+++ MyClass.h
@@ -1,1 +1,15 @@
 
+class MyClass
+   {
+     private:
+	int num;
+	string room;
+	bool fun;
+
+     public: //methods
+	void setNum(int);
+	int getNum();
+	void setRoom(string);
+	string getRoom();
+	void setFun(bool);
+	bool getFun();
